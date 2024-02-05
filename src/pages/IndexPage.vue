0e4834641abb625c4ddaf252704e79f89be3d484
Daniel Muench <d.muench@sap.com>
2024-02-05 08:50:51 +0100

    add todo list

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,46 +1,95 @@
 <template>
-  <q-page>
-    <div class="q-pa-md" style="max-width: 400px">
+  <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
       <q-input
-        ref="inputRef"
+        v-model="newTask"
+        @keyup.enter="addTask"
+        class="col"
+        square
         filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
-        :rules="[
-          (val) =>
-            (val && val.length > 0 && !val.match(/^\d/)) ||
-            'Please type something valid',
-        ]"
-        @update:model-value="modelUpdated()"
-        @blur="loseFocus()"
+        bg-color="white"
+        placeholder="Add task"
+        dense
       >
-        <template v-slot:prepend>
-          <q-icon name="font_download" />
+        <template v-slot:append>
+          <q-btn @click="addTask" round dense flat icon="add" />
         </template>
       </q-input>
-      <q-btn
-        class="q-mt-sm"
-        label="Reset Validation"
-        @click="reset"
-        color="primary"
-      />
+    </div>
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1': task.done }"
+        clickable
+        v-riple
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            v-model="task.done"
+            class="no-pointer-events"
+            color="primary"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            @click.stop="deleteTask(index)"
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div v-if="!tasks.length" class="no-tasks absolute-center">
+      <q-icon name="check" size="100px" color="primary"> </q-icon>
+      <div class="text-h5 text-primary text-center">No tasks</div>
     </div>
   </q-page>
 </template>
 
 <script setup>
+import { useQuasar } from "quasar";
 import { ref } from "vue";
-const name = ref("");
-const inputRef = ref(null);
+const newTask = ref("");
+const tasks = ref([]);
+const $q = useQuasar();
 
-function reset() {
-  inputRef.value.resetValidation();
+function deleteTask(index) {
+  $q.dialog({
+    title: "Confirm",
+    message: "Do you really want to delete the item?",
+    cancel: true,
+    persistent: true,
+  }).onOk(() => {
+    $q.notify(`Task ${tasks.value[index].title} deleted!`);
+    tasks.value.splice(index, 1);
+  });
 }
-function modelUpdated() {
-  console.log("I am typing...");
-}
-function loseFocus() {
-  console.log(inputRef.value.hasError);
+function addTask() {
+  tasks.value.push({
+    title: newTask.value,
+    done: false,
+  });
+  newTask.value = "";
 }
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+
+.no-tasks {
+  opacity: 0.5;
+}
+</style>
