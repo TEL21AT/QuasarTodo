20c37e4f06c092ad549b128a5de3b8ba73f75bd0
Daniel Muench <d.muench@sap.com>
2024-02-25 22:08:33 +0100

    add auth0

--- quasar.config.js
+++ quasar.config.js
@@ -27,7 +27,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [],
+    boot: ["auth0"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ["app.scss"],
---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <pre v-if="isAuthenticated">
+      <code>{{ user }}</code>
+    </pre>
+  </q-page>
+</template>
+
+<script setup>
+import { useAuth0 } from "@auth0/auth0-vue";
+
+const { user, isAuthenticated } = useAuth0();
+</script>
