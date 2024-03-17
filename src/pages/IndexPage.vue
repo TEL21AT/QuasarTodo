148729e09fe3f4fc8da16c2e951ba245ed260566
Tobias <tobias.joschko@gmx.de>
2024-03-17 21:27:48 +0100

    cleanup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -46,37 +46,11 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from "vue";
+import { ref } from "vue";
 import { useTodosStore } from "src/stores/todos";
-// import { useJwtStore } from "src/stores/jwt";
-import { useAuth0 } from "@auth0/auth0-vue";
 
 const { isAuthenticated, idTokenClaims } = useAuth0();
 const todosStore = useTodosStore();
-// const jwtStore = useJwtStore();
-
-// onMounted(() => {
-//   console.log("onMounted");
-//   console.log(isAuthenticated.value);
-//   if (isAuthenticated.value) {
-//     console.log("isAuthenticated is true");
-//     console.log(isAuthenticated.value);
-//     jwtStore.setToken(idTokenClaims.value.__raw);
-//   } else {
-//     console.log("isAuthenticated is false");
-//     jwtStore.clearToken();
-//   }
-// });
-// watch(isAuthenticated.value, (newValue) => {
-//   console.log("watching isAuthenticated value");
-//   if (newValue) {
-//     console.log("inside watch: " + isAuthenticated.value);
-//     jwtStore.setToken(idTokenClaims.value.__raw);
-//   } else {
-//     console.log("isAuthenticated is false");
-//     jwtStore.clearToken();
-//   }
-// });
 
 const newTodo = ref("");
 // const todos = ref([]);
